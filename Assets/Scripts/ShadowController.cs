8b20b3545408be25809738640652c4d536acb356
Wintercomo <chris.buter@live.nl>
2020-10-02 15:35:47 +0200

    added death screen

--- Assets/Scripts/ShadowController.cs
+++ Assets/Scripts/ShadowController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShadowController : MonoBehaviour
 {
@@ -10,11 +11,14 @@ public class ShadowController : MonoBehaviour
     private Vector3 startPoint;
     private float distanceBetweenStartAndEnd;
     private float StartDistance;
-    
+    GameObject rawImage;
+
 
     // Start is called before the first frame 
     void Start()
     {
+        rawImage = GameObject.Find("RawImage");
+        rawImage.SetActive(false);
         startPoint = transform.position;
         distanceBetweenStartAndEnd = Vector3.Distance(startPoint, endPointShadow.transform.position);
         StartDistance = Vector3.Distance(Player.transform.position, follower.transform.position);
@@ -26,10 +30,19 @@ public class ShadowController : MonoBehaviour
         var curDist = Vector3.Distance(Player.transform.position, follower.transform.position);
         if(curDist < StartDistance){
             var percent = curDist / (StartDistance / 100);
+            Debug.Log(percent);
+            if (percent <= 20)
+            {
+                //load image
+                rawImage.SetActive(true);
+                //rawImage.GetComponent<RawImage>().IsActive();
+            }
+            else
+            {
+                var upDist = (distanceBetweenStartAndEnd / 100) * (100 - percent);
 
-            var upDist = (distanceBetweenStartAndEnd / 100) * (100 - percent);
-
-            transform.position = new Vector3(Player.transform.position.x, startPoint.y + upDist, Player.transform.position.z);
+                transform.position = new Vector3(Player.transform.position.x, startPoint.y + upDist, Player.transform.position.z);
+            }
         }
     }
 }
