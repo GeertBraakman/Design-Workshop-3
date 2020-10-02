b3aa796d77592bc33be80eb3b3e3ffad09b4ca25
Wintercomo <chris.buter@live.nl>
2020-10-02 14:10:05 +0200

    Added npc that wonders

---
+++ Assets/NPCMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCMovement : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public float speedIncrease = 1;
+    public float walkRadius = 5;
+    public bool DoWaypoints = true;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (DoWaypoints == true)
+        {
+            float dist = GetComponent<NavMeshAgent>().remainingDistance;
+            if (dist != Mathf.Infinity && GetComponent<NavMeshAgent>().pathStatus == NavMeshPathStatus.PathComplete && GetComponent<NavMeshAgent>().remainingDistance < 5)//Arrived.
+            {
+                Debug.Log("Ägent has arrived");
+                Wonder();
+            }
+        }
+    }
+    void Wonder()
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * walkRadius;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randomDirection, out hit, walkRadius, 1);
+        Vector3 finalPosition = hit.position;
+        GetComponent<NavMeshAgent>().destination = finalPosition;
+    }
+}
